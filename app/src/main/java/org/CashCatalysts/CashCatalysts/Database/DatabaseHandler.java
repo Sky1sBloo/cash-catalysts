0d35ad299a279a34b0e36a537b2056a7f581ab25
Sky1sBloo <ultimate8gaming@gmail.com>
2025-01-07 13:19:04 +0800

    feat: Added perform query function

--- app/src/main/java/org/CashCatalysts/CashCatalysts/Database/DatabaseHandler.java
+++ app/src/main/java/org/CashCatalysts/CashCatalysts/Database/DatabaseHandler.java
@@ -4,14 +4,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Class to handle database connections
+ */
 public class DatabaseHandler {
-    private Connection connection;
+    private final Connection connection;
 
     /**
      * Path to database
+     * Automatically includes the path to the database
      */
     public DatabaseHandler(String pathToDb) throws SQLException {
         String url = "jdbc:sqlite:pathToDb" + pathToDb;
         connection = DriverManager.getConnection(url);
+    }
+
+    public void performQuery(String query) throws SQLException {
+        connection.createStatement().execute(query);
     }
 }
