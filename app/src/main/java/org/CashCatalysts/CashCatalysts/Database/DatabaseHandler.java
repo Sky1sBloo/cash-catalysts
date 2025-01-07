e906bc9f91358581158a916163f156dffdf0e466
Sky1sBloo <ultimate8gaming@gmail.com>
2025-01-07 16:05:39 +0800

    feat: Fixed constructor using wrong

--- app/src/main/java/org/CashCatalysts/CashCatalysts/Database/DatabaseHandler.java
+++ app/src/main/java/org/CashCatalysts/CashCatalysts/Database/DatabaseHandler.java
@@ -5,20 +5,25 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- * Class to handle database connections
+ * Class to handle database connections and tables
  */
 public class DatabaseHandler {
     private final Connection connection;
 
     /**
-     * Path to database
-     * Automatically includes the path to the database
+     * Path to database file
+     * Note: Automatically adds the jdbc prefix
      */
     public DatabaseHandler(String pathToDb) throws SQLException {
-        String url = "jdbc:sqlite:pathToDb" + pathToDb;
+        String url = "jdbc:sqlite:" + pathToDb;
         connection = DriverManager.getConnection(url);
     }
 
+    /**
+     * Performs a generic non returnable query
+     * @param query
+     * @throws SQLException
+     */
     public void performQuery(String query) throws SQLException {
         connection.createStatement().execute(query);
     }
