b5ec5399a944e89454a8c1929624146628311e60
Sky1sBloo <ultimate8gaming@gmail.com>
2025-01-09 18:31:32 +0800

    feat: added initial users table implementation

---
+++ app/src/main/java/org/CashCatalysts/CashCatalysts/Database/UsersTable.java
@@ -0,0 +1,37 @@
+package org.CashCatalysts.CashCatalysts.Database;
+
+import org.CashCatalysts.CashCatalysts.UserProfile.User;
+
+import java.sql.*;
+
+public class UsersTable extends DbTable {
+    public UsersTable(Connection connection) throws SQLException {
+        super(connection);
+
+        DbField[] fields = {
+                new DbField("user_id", "INT", "PRIMARY KEY AUTO INCREMENT"),
+                new DbField("username", "VARCHAR(255)", "NOT NULL"),
+                new DbField("rank", "INT", "NOT NULL")
+        };
+        super.createTable("users", fields);
+    }
+
+    /**
+     * Registers the user in the database
+     *
+     * @param user Only requires the username and the rank
+     * @return The id of the user
+     */
+    public void registerUser(User user) throws SQLException {
+        String sql = "INSERT INTO users (username, rank) VALUES(?, ?);";
+        PreparedStatement insertStatement = connection.prepareStatement(sql);
+
+        insertStatement.setString(1, user.username());
+        insertStatement.setInt(2, user.rank());
+        insertStatement.executeUpdate();
+
+        Statement lastIdStatement = connection.createStatement();
+
+        ResultSet rs = lastIdStatement.executeQuery("select last_insert_rowid();");
+    }
+}
