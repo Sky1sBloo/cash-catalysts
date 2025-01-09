b5a413e20e70c083affb4e100e5f06a695fc0bf8
Sky1sBloo <ultimate8gaming@gmail.com>
2025-01-09 19:46:42 +0800

    feat: added create user that also for update

--- app/src/main/java/org/CashCatalysts/CashCatalysts/UserProfile/UserProfileHandler.java
+++ app/src/main/java/org/CashCatalysts/CashCatalysts/UserProfile/UserProfileHandler.java
@@ -4,16 +4,16 @@ public class UserProfileHandler {
     /**
      * Create new user
      *
-     * @param userID identifier of user
+     * @param userID   identifier of user
      * @param username name of user
-     * @param rank rank of user; can't be < 1
+     * @param rank     rank of user; can't be < 1
      * @return a {@code User} object representing the created user
      */
-    public static User createUser(int userID, String username, int rank){
-        if(username == null || username.isBlank()){
+    public static User createUser(int userID, String username, int rank) {
+        if (username == null || username.isBlank()) {
             throw new IllegalArgumentException("Fill in username.");
         }
-        if(rank < 1){
+        if (rank < 1) {
             throw new IllegalArgumentException("Rank can't be negative.");
         }
         return new User(userID, username, rank);
@@ -23,10 +23,20 @@ public class UserProfileHandler {
      * Creates a new user with default rank
      * Note: Generally used for registering new users
      */
-    public static User createUser(String username){
-        if(username == null || username.isBlank()){
+    public static User createUser(String username) {
+        if (username == null || username.isBlank()) {
             throw new IllegalArgumentException("Fill in username.");
         }
         return new User(null, username, 1);
+    }
+
+    /**
+     * Create a new user with for updating
+     */
+    public static User createUser(String username, int rank) {
+        if (username == null || username.isBlank()) {
+            throw new IllegalArgumentException("Fill in username.");
+        }
+        return new User(null, username, rank);
     }
 }
