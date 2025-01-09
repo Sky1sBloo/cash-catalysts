8ddba1e0fd7b65640051f77923295b635fd7b225
Sky1sBloo <ultimate8gaming@gmail.com>
2025-01-09 20:22:03 +0800

    feat: Add transaction is implemented

--- app/src/main/java/org/CashCatalysts/CashCatalysts/Database/TransactionsTable.java
+++ app/src/main/java/org/CashCatalysts/CashCatalysts/Database/TransactionsTable.java
@@ -1,6 +1,10 @@
 package org.CashCatalysts.CashCatalysts.Database;
 
+import org.CashCatalysts.CashCatalysts.Transactions.Transaction;
+
+import java.math.BigDecimal;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 /**
@@ -16,7 +20,7 @@ public class TransactionsTable extends DbTable {
     public TransactionsTable(Connection connection) throws SQLException {
         super(connection);
         DbField[] fields = {
-                new DbField("transaction_id", "INT", "PRIMARY KEY"),
+                new DbField("transaction_id", "INTEGER", "PRIMARY KEY AUTOINCREMENT"),
                 new DbField("name", "VARCHAR(255)"),
                 new DbField("type", "VARCHAR(255)", "NOT NULL"),
                 new DbField("date", "DATE", "NOT NULL"),
@@ -25,7 +29,21 @@ public class TransactionsTable extends DbTable {
         super.createTable("transactions", fields);
     }
 
-    // Todo: Will accept a record representing a transaction
-    public void addTransaction() {
+    /**
+     * Adds a new transaction to database
+     * Note: Converts the transaction amount to cents
+     */
+    public void addTransaction(Transaction transaction) throws SQLException {
+        String sql = "INSERT INTO transactions (name, type, date, amount_cents) VALUES(?, ?, ?, ?);";
+        PreparedStatement addStatement = connection.prepareStatement(sql);
+
+        addStatement.setString(1, transaction.name());
+        addStatement.setString(2, transaction.type());
+        addStatement.setDate(3, transaction.date());
+        // Get decimal part
+        int amountCents = transaction.amount() * 100 + transaction.amountCents();
+        addStatement.setInt(4, amountCents);
+
+        addStatement.executeUpdate();
     }
 }
