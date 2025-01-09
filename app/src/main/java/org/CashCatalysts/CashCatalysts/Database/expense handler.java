d2b3bb2b1ca209a146845ac128d0b44a2197d97a
chrismacharis <sbcv.charisdelacruz@gmail.com>
2025-01-09 18:19:02 +0800

    expense handler

--- app/src/main/java/org/CashCatalysts/CashCatalysts/Database/expense handler.java
+++ app/src/main/java/org/CashCatalysts/CashCatalysts/Database/expense handler.java
@@ -1,43 +1,9 @@
+package org.CashCatalysts.CashCatalysts.Expenses;
+
 import java.util.ArrayList;
 import java.util.List;
 
-class Transaction {
-    private int transactionId;
-    private String expenseOrIncome;
-    private String name;
-    private String type;
-    private String date;
-    private double amount;
-
-    public Transaction(int transactionId, String expenseOrIncome, String name, String type, String date, double amount) {
-        this.transactionId = transactionId;
-        this.expenseOrIncome = expenseOrIncome;
-        this.name = name;
-        this.type = type;
-        this.date = date;
-        this.amount = amount;
-    }
-
-    public int getTransactionId() {
-        return transactionId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    @Override
-    public String toString() {
-        return "TransactionID: " + transactionId + ", Expense/Income: " + expenseOrIncome +
-               ", Name: " + name + ", Type: " + type + ", Date: " + date + ", Amount: " + amount;
-    }
-}
-
-public class Handler {
+public class ExpenseHandler {
     private List<Transaction> transactions = new ArrayList<>();
     private int currentId = 1; // To generate unique transaction IDs
 
@@ -52,11 +18,10 @@ public class Handler {
 
     public Transaction getTransactionById(int id) {
         for (Transaction transaction : transactions) {
-            if (transaction.getTransactionId() == id) {
+            if (transaction.transactionId() == id) {
                 return transaction;
             }
         }
         return null;
     }
-
-
+}
