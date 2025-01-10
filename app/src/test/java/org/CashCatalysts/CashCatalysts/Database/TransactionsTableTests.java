91b6aad8e23419bbef81b9d7761c29df4edf3590
Sky1sBloo <ultimate8gaming@gmail.com>
2025-01-10 09:46:21 +0800

    test: Added delete transaction tests

--- app/src/test/java/org/CashCatalysts/CashCatalysts/Database/TransactionsTableTests.java
+++ app/src/test/java/org/CashCatalysts/CashCatalysts/Database/TransactionsTableTests.java
@@ -63,4 +63,24 @@ public class TransactionsTableTests {
         List<Transaction> actualTransactions = transactionsTable.getAllTransactionsBetween(Date.valueOf("2023-05-01"), Date.valueOf("2023-06-01"));
         Assertions.assertArrayEquals(expectedTransactions, actualTransactions.toArray());
     }
+
+    @Test
+    public void deleteTransactionTest() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:sqlite::memory:");
+        TransactionsTable transactionsTable = new TransactionsTable(connection);
+
+        Transaction transaction1 = TransactionHandler.createTransaction("oldDate", "type1", Date.valueOf("2023-01-01"), 100, 0);
+        Transaction transaction2 = TransactionHandler.createTransaction("slightDate", "type2", Date.valueOf("2023-05-13"), 200, 25);
+        Transaction transaction3 = TransactionHandler.createTransaction("newDate", "testType", Date.valueOf("2023-06-01"), 300, 50);
+        Transaction transaction4 = TransactionHandler.createTransaction("futureDate", "testType", Date.valueOf("2023-12-31"), 400, 75);
+
+        transactionsTable.addTransaction(transaction1);
+        int transaction2Id = transactionsTable.addTransaction(transaction2);
+        transactionsTable.addTransaction(transaction3);
+        transactionsTable.addTransaction(transaction4);
+
+        transactionsTable.deleteTransaction(2);
+
+        Assertions.assertThrows(SQLException.class, () -> transactionsTable.getTransaction(transaction2Id));
+    }
 }
