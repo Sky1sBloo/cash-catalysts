6e8be877dc3ce9726bf90852a267e28e9e23f11b
Sky1sBloo <ultimate8gaming@gmail.com>
2025-01-19 21:29:09 +0800

    feat: Added delete and edit goals

--- app/src/main/java/org/CashCatalysts/CashCatalysts/controllers/GoalsController.java
+++ app/src/main/java/org/CashCatalysts/CashCatalysts/controllers/GoalsController.java
@@ -6,8 +6,11 @@ import javafx.scene.control.*;
 import javafx.stage.StageStyle;
 import org.CashCatalysts.CashCatalysts.GoalsSavings.Goal;
 import org.CashCatalysts.CashCatalysts.GoalsSavings.GoalsHandler;
+import org.CashCatalysts.CashCatalysts.GoalsSavings.GoalsType;
+import org.CashCatalysts.CashCatalysts.datatypes.Currency;
 
 import java.io.IOException;
+import java.sql.Date;
 import java.util.List;
 
 public class GoalsController {
@@ -24,11 +27,32 @@ public class GoalsController {
 
     public GoalsController(GoalsHandler goalsHandler) {
         this.goalsHandler = goalsHandler;
+        // Add some test goals
+        goalsHandler.addGoal(GoalsHandler.createGoal("Test goal 1", new Currency(1000, 20), Date.valueOf("2021-10-21"), GoalsType.CHARITY));
+        goalsHandler.addGoal(GoalsHandler.createGoal("Test goal 2", new Currency(2000, 0), Date.valueOf("2021-12-21"), GoalsType.SAVINGS));
+        goalsHandler.addGoal(GoalsHandler.createGoal("Test goal 3", new Currency(3000, 0), Date.valueOf("2021-11-21"), GoalsType.INVESTMENT));
+
     }
 
     @SuppressWarnings("unused")
     public void initialize() {
         add_goal_btn.setOnAction((ignore) -> addGoal());
+        edit_goal_btn.setOnAction((ignore) -> {
+            int selectedIndex = goals_list.getSelectionModel().getSelectedIndex();
+            if (selectedIndex == -1) {
+                return;
+            }
+            Goal toEdit = goalsHandler.getAllGoals().get(selectedIndex);
+            addGoal(toEdit);
+        });
+        delete_goal_btn.setOnAction((ignore) -> {
+            int selectedIndex = goals_list.getSelectionModel().getSelectedIndex();
+            if (selectedIndex == -1) {
+                return;
+            }
+            goalsHandler.deleteGoal(goalsHandler.getAllGoals().get(selectedIndex).id());
+            refresh();
+        });
         refresh();
     }
 
@@ -45,7 +69,7 @@ public class GoalsController {
     }
 
     private String formatGoal(Goal goal) {
-        return goal.name() + "  -  " + goal.amount().getAmount() + "  -  " + goal.deadline();
+        return goal.type() + "  -  " + goal.name() + "  -  " + goal.amount().getAmount() + "  -  " + goal.deadline();
     }
 
     private void addGoal(Goal toEdit) {
