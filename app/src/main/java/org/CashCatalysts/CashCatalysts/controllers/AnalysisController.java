f937a79f18a638be3f3770d6e970ca811d26bd66
Sky1sBloo <ultimate8gaming@gmail.com>
2025-01-23 17:45:46 +0800

    feat: Added yearly activity line chart

--- app/src/main/java/org/CashCatalysts/CashCatalysts/controllers/AnalysisController.java
+++ app/src/main/java/org/CashCatalysts/CashCatalysts/controllers/AnalysisController.java
@@ -1,11 +1,14 @@
 package org.CashCatalysts.CashCatalysts.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import org.CashCatalysts.CashCatalysts.UserStats.UserStatsSystem;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.Map;
 
 public class AnalysisController {
     private final UserStatsSystem userStatsSystem;
@@ -14,6 +17,8 @@ public class AnalysisController {
     private Label highest_category_monthly_lbl;
     @FXML
     private Label comparison_to_last_month_lbl;
+    @FXML
+    private LineChart<String, Integer> yearly_activity_linechart;
 
     public AnalysisController(UserStatsSystem userStatsSystem) {
         this.userStatsSystem = userStatsSystem;
@@ -23,6 +28,7 @@ public class AnalysisController {
     public void initialize() {
         loadHighestCategory();
         loadComparisonToLastMonth();
+        loadYearlyActivity();
     }
 
     private void loadHighestCategory() {
@@ -46,5 +52,24 @@ public class AnalysisController {
         LocalDate startOfMonth = now.withDayOfMonth(1);
         double lastMonthComparison = userStatsSystem.compareToLastMonth(Date.valueOf(startOfMonth));
         comparison_to_last_month_lbl.setText(String.valueOf(lastMonthComparison));
+    }
+
+    private void loadYearlyActivity() {
+        LocalDate now = LocalDate.now();
+        LocalDate startOfYear = now.withDayOfYear(1);
+        LocalDate endOfYear = now.withDayOfYear(now.lengthOfYear());
+
+        var yearlyExpenseBreakdown = userStatsSystem.getMonthlyExpenseBreakdown(
+                Date.valueOf(startOfYear),
+                Date.valueOf(endOfYear)
+        );
+        yearly_activity_linechart.getData().clear();
+
+        XYChart.Series<String, Integer> series = new XYChart.Series<>();
+        series.setName("Yearly Expenses");
+        for (Map.Entry<String, Integer> entry : yearlyExpenseBreakdown.entrySet()) {
+            series.getData().add(new XYChart.Data<>(String.valueOf(entry.getKey()), entry.getValue()));
+        }
+        yearly_activity_linechart.getData().add(series);
     }
 }
