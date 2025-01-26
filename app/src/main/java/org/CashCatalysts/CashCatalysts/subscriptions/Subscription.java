0b0f4b22ee98de551a2bc4f419f9dad6327b6842
Sky1sBloo <ultimate8gaming@gmail.com>
2025-01-26 20:06:06 +0800

    feat: Made subscription id nullable and added create subscription without id

--- app/src/main/java/org/CashCatalysts/CashCatalysts/subscriptions/Subscription.java
+++ app/src/main/java/org/CashCatalysts/CashCatalysts/subscriptions/Subscription.java
@@ -4,7 +4,7 @@ import org.CashCatalysts.CashCatalysts.datatypes.Currency;
 
 import java.time.LocalDate;
 
-public record Subscription(int id, String name, String type, SubscriptionFrequency frequency, LocalDate startDate, LocalDate endDate, Currency amount) {
+public record Subscription(Integer id, String name, String type, SubscriptionFrequency frequency, LocalDate startDate, LocalDate endDate, Currency amount) {
     @Override
     public String toString() {
         return "SubscriptionRecord{id=" + id + ", name='" + name + "', type='" + type + "', paymentTime='" + frequency+ "', startDate=" + startDate + ", endDate=" + endDate + ", " + amount.toString() + '}';
--- app/src/main/java/org/CashCatalysts/CashCatalysts/subscriptions/SubscriptionsHandler.java
+++ app/src/main/java/org/CashCatalysts/CashCatalysts/subscriptions/SubscriptionsHandler.java
@@ -1,59 +1,83 @@
 package org.CashCatalysts.CashCatalysts.subscriptions;
 
 import org.CashCatalysts.CashCatalysts.Database.DatabaseHandler;
+import org.CashCatalysts.CashCatalysts.Database.SubscriptionsTable;
+import org.CashCatalysts.CashCatalysts.Transactions.Transaction;
 import org.CashCatalysts.CashCatalysts.Transactions.TransactionHandler;
+import org.CashCatalysts.CashCatalysts.datatypes.Currency;
 import org.CashCatalysts.CashCatalysts.datatypes.DateRange;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.List;
 
 public class SubscriptionsHandler {
-    private final DatabaseHandler dbHandler;
+    private final SubscriptionsTable subscriptionsTable;
     private final TransactionHandler transactionHandler;
 
     public SubscriptionsHandler(DatabaseHandler dbHandler, TransactionHandler transactionHandler) {
-        this.dbHandler = dbHandler;
+        this.subscriptionsTable = dbHandler.getSubscriptionsTable();
         this.transactionHandler = transactionHandler;
     }
 
+    public static Subscription createSubscription(String name, String type, SubscriptionFrequency frequency, LocalDate startDate, LocalDate endDate, Currency amount) {
+        return new Subscription(null, name, type, frequency, startDate, endDate, amount);
+    }
+
     public void addSubscription(Subscription subscription) throws SQLException {
-        dbHandler.getSubscriptionsTable().insertSubscription(subscription);
+        subscriptionsTable.insertSubscription(subscription);
         addTransactionForSubscription(subscription);
     }
 
-    public void editSubscription(Subscription subscription) throws SQLException {
-        dbHandler.getSubscriptionsTable().editSubscription(subscription);
+    public Subscription getSubscription(int id) throws SQLException {
+        return subscriptionsTable.getSubscription(id);
     }
 
-    public void deleteSubscription(int id) throws SQLException {
-        dbHandler.getSubscriptionsTable().deleteSubscription(id);
+    public void editSubscription(Subscription subscription) throws SQLException {
+        subscriptionsTable.editSubscription(subscription);
+    }
+
+    /**
+     * Deletes a subscription and all transactions after startDeletion associated with it
+     */
+    public void deleteSubscription(int id, LocalDate startDeletion) throws SQLException {
+        subscriptionsTable.deleteSubscription(id);
+        deleteAllTransactionsOnSubscriptionAfterDate(getSubscription(id), startDeletion);
     }
 
     public ResultSet filterSubscriptionsByDateAndType(String type, DateRange dateRange) throws SQLException {
-        return dbHandler.getSubscriptionsTable().filterSubscriptionsByDateAndType(type, dateRange.begin(), dateRange.end());
+        return subscriptionsTable.filterSubscriptionsByDateAndType(type, dateRange.begin(), dateRange.end());
     }
 
     private void addTransactionForSubscription(Subscription subscription) {
         LocalDate currentDate = subscription.startDate();
     }
 
-    /*
-    public void addTransactionsForSubscriptions() throws SQLException {
-        ResultSet subscriptions = dbHandler.getSubscriptionsTable().getAllSubscriptions();
-        while (subscriptions.next()) {
-            int id = subscriptions.getInt("id");
-            LocalDate startDate = subscriptions.getDate("startDate").toLocalDate();
-            LocalDate endDate = subscriptions.getDate("endDate").toLocalDate();
-            String frequencyStr = subscriptions.getString("paymentTime");
-            SubscriptionFrequency frequency = SubscriptionFrequency.valueOf(frequencyStr);
+    public void addTransactionsForSubscriptions(Subscription subscription) {
+        LocalDate currentDate = subscription.startDate();
 
-
-            // Automatically add transactions based on the subscription details
-            System.out.println("Processing subscription ID: " + id + " from " + startDate + " to " + endDate + " with payment frequency: " + frequencyStr);
-
-            // Add transactions for this subscription
-            dbHandler.getSubscriptionsTable().addTransactionsForSubscription(id, startDate, endDate, frequency);
+        while (currentDate.isBefore(subscription.endDate())) {
+            if (transactionHandler.getTransactionOnSubscriptionWithDate(subscription, currentDate) == null) {
+                continue;
+            }
+            transactionHandler.addTransaction(TransactionHandler.createTransaction(subscription.name(), subscription.type(), currentDate, subscription.amount(), subscription.id()));
+            currentDate = switch (subscription.frequency()) {
+                case DAILY -> currentDate.plusDays(1);
+                case WEEKLY -> currentDate.plusWeeks(1);
+                case BIWEEKLY -> currentDate.plusWeeks(2);
+                case MONTHLY -> currentDate.plusMonths(1);
+                case YEARLY -> currentDate.plusYears(1);
+            };
         }
-    } */
+    }
+
+    public void deleteAllTransactionsOnSubscriptionAfterDate(Subscription subscription, LocalDate date) {
+        List<Transaction> transactions = transactionHandler.getAllTransactionsOnSubscription(subscription);
+        transactions.stream()
+                .filter(transaction -> transaction.date().isAfter(date))
+                .forEach(transaction -> {
+                    transactionHandler.deleteTransaction(transaction.transactionId());
+                });
+    }
 }
