f7a44e1d9d63172300d4bdc0b7f2058dc35ce6b2
Sky1sBloo <ultimate8gaming@gmail.com>
2025-01-28 10:59:11 +0800

    refactor: Made challenge reward use int

--- app/src/main/java/org/CashCatalysts/CashCatalysts/game/challenges/ChallengeReward.java
+++ app/src/main/java/org/CashCatalysts/CashCatalysts/game/challenges/ChallengeReward.java
@@ -2,5 +2,5 @@ package org.CashCatalysts.CashCatalysts.game.challenges;
 
 import org.CashCatalysts.CashCatalysts.game.currency.GameCurrency;
 
-public record ChallengeReward(GameCurrency gold, GameCurrency star, int xp) {
+public record ChallengeReward(int gold, int star, int xp) {
 }
