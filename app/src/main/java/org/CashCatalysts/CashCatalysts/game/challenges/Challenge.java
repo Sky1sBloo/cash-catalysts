7c70799f50d496fdaf19436cec1433aedb3587d2
Sky1sBloo <ultimate8gaming@gmail.com>
2025-01-28 11:03:10 +0800

    feat: Added start date and made member variables protected

--- app/src/main/java/org/CashCatalysts/CashCatalysts/game/challenges/Challenge.java
+++ app/src/main/java/org/CashCatalysts/CashCatalysts/game/challenges/Challenge.java
@@ -5,15 +5,17 @@ import org.CashCatalysts.CashCatalysts.game.currency.GameCurrency;
 import java.time.LocalDate;
 
 public abstract class Challenge {
-    private final String name;
-    private final String description;
-    ChallengeReward reward;
-    private final LocalDate deadline;
+    protected final String name;
+    protected final String description;
+    protected ChallengeReward reward;
+    protected final LocalDate startDate;
+    protected final LocalDate deadline;
 
-    public Challenge(String name, String description, ChallengeReward reward, LocalDate deadline) {
+    public Challenge(String name, String description, ChallengeReward reward, LocalDate startDate, LocalDate deadline) {
         this.name = name;
         this.description = description;
         this.reward = reward;
+        this.startDate = startDate;
         this.deadline = deadline;
     }
 
@@ -27,6 +29,14 @@ public abstract class Challenge {
 
     public ChallengeReward getReward() {
         return reward;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
     }
 
     public abstract boolean conditionMet();
