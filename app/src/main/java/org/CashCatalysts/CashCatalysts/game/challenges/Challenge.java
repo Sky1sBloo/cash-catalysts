fe3b9c907c35801a8376439c7755239addb40798
Sky1sBloo <ultimate8gaming@gmail.com>
2025-01-28 10:54:47 +0800

    feat: Added deadline on challenge

--- app/src/main/java/org/CashCatalysts/CashCatalysts/game/challenges/Challenge.java
+++ app/src/main/java/org/CashCatalysts/CashCatalysts/game/challenges/Challenge.java
@@ -2,15 +2,19 @@ package org.CashCatalysts.CashCatalysts.game.challenges;
 
 import org.CashCatalysts.CashCatalysts.game.currency.GameCurrency;
 
+import java.time.LocalDate;
+
 public abstract class Challenge {
     private final String name;
     private final String description;
     ChallengeReward reward;
+    private final LocalDate deadline;
 
-    public Challenge(String name, String description, ChallengeReward reward) {
+    public Challenge(String name, String description, ChallengeReward reward, LocalDate deadline) {
         this.name = name;
         this.description = description;
         this.reward = reward;
+        this.deadline = deadline;
     }
 
     public String getName() {
@@ -19,6 +23,10 @@ public abstract class Challenge {
 
     public String getDescription() {
         return description;
+    }
+
+    public ChallengeReward getReward() {
+        return reward;
     }
 
     public abstract boolean conditionMet();
