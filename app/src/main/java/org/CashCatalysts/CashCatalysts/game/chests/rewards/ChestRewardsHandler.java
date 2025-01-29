833991775cf6895f8e9288b837bbc5d3562517c5
Sky1sBloo <ultimate8gaming@gmail.com>
2025-01-29 13:53:00 +0800

    feat: Updated chest rewards to be more forgiving

--- app/src/main/java/org/CashCatalysts/CashCatalysts/game/chests/rewards/ChestRewardsHandler.java
+++ app/src/main/java/org/CashCatalysts/CashCatalysts/game/chests/rewards/ChestRewardsHandler.java
@@ -9,36 +9,36 @@ import java.util.List;
 
 public class ChestRewardsHandler {
     private static final ChestRewardRarity[] normalRewards = {
-            new ChestRewardRarity(Plant.BANANA, 30),
-            new ChestRewardRarity(Plant.PINEAPPLE, 5),
-            new ChestRewardRarity(Plant.STRAWBERRY, 5),
-            new ChestRewardRarity(Plant.APPLE, 30),
-            new ChestRewardRarity(Plant.SAMPAGUITA, 21),
-            new ChestRewardRarity(Plant.ORCHIDS, 2),
-            new ChestRewardRarity(Plant.SUNFLOWER, 5),
-            new ChestRewardRarity(Plant.ROSE, 2)
-    };
-
-    private static final ChestRewardRarity[] rareRewards = {
-            new ChestRewardRarity(Plant.BANANA, 20),
-            new ChestRewardRarity(Plant.PINEAPPLE, 10),
-            new ChestRewardRarity(Plant.STRAWBERRY, 10),
+            new ChestRewardRarity(Plant.BANANA, 80),
+            new ChestRewardRarity(Plant.PINEAPPLE, 60),
+            new ChestRewardRarity(Plant.STRAWBERRY, 40),
             new ChestRewardRarity(Plant.APPLE, 20),
-            new ChestRewardRarity(Plant.SAMPAGUITA, 20),
-            new ChestRewardRarity(Plant.ORCHIDS, 5),
+            new ChestRewardRarity(Plant.SAMPAGUITA, 51),
+            new ChestRewardRarity(Plant.ORCHIDS, 20),
             new ChestRewardRarity(Plant.SUNFLOWER, 10),
             new ChestRewardRarity(Plant.ROSE, 5)
     };
 
-    private static final ChestRewardRarity[] epicRewards = {
-            new ChestRewardRarity(Plant.BANANA, 15),
-            new ChestRewardRarity(Plant.PINEAPPLE, 13),
-            new ChestRewardRarity(Plant.STRAWBERRY, 12),
-            new ChestRewardRarity(Plant.APPLE, 15),
-            new ChestRewardRarity(Plant.SAMPAGUITA, 15),
-            new ChestRewardRarity(Plant.ORCHIDS, 10),
-            new ChestRewardRarity(Plant.SUNFLOWER, 10),
+    private static final ChestRewardRarity[] rareRewards = {
+            new ChestRewardRarity(Plant.BANANA, 60),
+            new ChestRewardRarity(Plant.PINEAPPLE, 50),
+            new ChestRewardRarity(Plant.STRAWBERRY, 60),
+            new ChestRewardRarity(Plant.APPLE, 40),
+            new ChestRewardRarity(Plant.SAMPAGUITA, 30),
+            new ChestRewardRarity(Plant.ORCHIDS, 40),
+            new ChestRewardRarity(Plant.SUNFLOWER, 20),
             new ChestRewardRarity(Plant.ROSE, 10)
+    };
+
+    private static final ChestRewardRarity[] epicRewards = {
+            new ChestRewardRarity(Plant.BANANA, 30),
+            new ChestRewardRarity(Plant.PINEAPPLE, 40),
+            new ChestRewardRarity(Plant.STRAWBERRY, 70),
+            new ChestRewardRarity(Plant.APPLE, 30),
+            new ChestRewardRarity(Plant.SAMPAGUITA, 10),
+            new ChestRewardRarity(Plant.ORCHIDS, 60),
+            new ChestRewardRarity(Plant.SUNFLOWER, 40),
+            new ChestRewardRarity(Plant.ROSE, 20)
     };
 
     private final ChestRarity rarity;
@@ -63,7 +63,7 @@ public class ChestRewardsHandler {
 
         for (ChestRewardRarity reward : rewardsArray) {
             if (ApplicationRandom.randomInt(0, 100) < reward.rarity()) {
-                rewards.add(new ChestDrop(reward.plant(), 1));
+                rewards.add(new ChestDrop(reward.plant(), amount));
             }
         }
         return rewards;
